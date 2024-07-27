9eb00974b4d5bf5a55aea2af7b581640c3c33eed
NickoalsPogozelski <nickolas@oneweb.com.br>
2024-07-27 10:04:23 -0300

    Ajustes Finais

--- cms/src/Controller/ArticlesController.php
+++ cms/src/Controller/ArticlesController.php
@@ -10,13 +10,12 @@ class ArticlesController extends AppController
     {
         parent::initialize();
 
-        $this->loadComponent('Paginator');
         $this->loadComponent('Flash'); // Include the FlashComponent
     }
 
     public function index()
     {
-        $articles = $this->Paginator->paginate($this->Articles->find());
+        $articles = $this->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
 
@@ -61,5 +60,16 @@ class ArticlesController extends AppController
         }
 
         $this->set('article', $article);
+    }
+
+    public function delete($slug)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        if ($this->Articles->delete($article)) {
+            $this->Flash->success(__('The {0} article has been deleted.', $article->title));
+            return $this->redirect(['action' => 'index']);
+        }
     }
 }
